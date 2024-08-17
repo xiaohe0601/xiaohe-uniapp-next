71961a47acabf437f99fc35aaa873255079f3591
xiaohe0601 <HeDianGeng0601@outlook.com>
2024-08-17 19:10:40 +0800

    chore: remove redundant types
    (cherry picked from commit 78fa06525f8855628111b0e1cd2cc6e3b8ae1dcd)

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,35 @@
 import path from "node:path";
 import UniApp from "@dcloudio/vite-plugin-uni";
-import UniHelperComponents from "@uni-helper/vite-plugin-uni-components";
-import UniHelperManifest from "@uni-helper/vite-plugin-uni-manifest";
-import UniHelperPages from "@uni-helper/vite-plugin-uni-pages";
+import UniComponents from "@uni-helper/vite-plugin-uni-components";
+import UniManifest from "@uni-helper/vite-plugin-uni-manifest";
+import UniPages from "@uni-helper/vite-plugin-uni-pages";
 import UnoCSS from "unocss/vite";
 import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig, loadEnv } from "vite";
 import { NutResolver } from "nutui-uniapp";
 
-function resolve(dir: string): string {
-  return path.resolve(process.cwd(), ".", dir);
+function r(...paths: string[]) {
+  return path.resolve(process.cwd(), ".", ...paths);
 }
 
 export default defineConfig(({ mode }) => {
   const env: Record<string, string> = loadEnv(mode, process.cwd());
 
-  const compress: boolean = ["production", "preview"].includes(mode);
+  const compress = ["production", "preview"].includes(mode);
 
   return {
     resolve: {
       alias: {
-        "@": resolve("src")
+        "@": r("src")
       }
     },
     plugins: [
-      UniHelperManifest(),
-      UniHelperPages({
+      UniManifest(),
+      UniPages({
         dts: "types/uni-pages.d.ts",
         mergePages: false
       }),
-      UniHelperComponents({
+      UniComponents({
         dts: "types/components.d.ts",
         dirs: ["src/components"],
         directoryAsNamespace: true,
