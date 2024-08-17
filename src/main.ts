78fa06525f8855628111b0e1cd2cc6e3b8ae1dcd
xiaohe0601 <HeDianGeng0601@outlook.com>
2024-08-17 19:10:40 +0800

    chore: remove redundant types

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import type { App as VueApp } from "vue";
 import { createSSRApp } from "vue";
 import * as Pinia from "pinia";
-import App from "@/App.vue";
+import App from "./App.vue";
 import "virtual:uno.css";
 import "@/assets/xh-iconfont/xh-iconfont.css";
 import "@/assets/iconfont/iconfont.css";
@@ -11,7 +10,7 @@ import "@/plugins/vconsole.ts";
 // #endif
 
 export function createApp() {
-  const app: VueApp<Element> = createSSRApp(App);
+  const app = createSSRApp(App);
 
   app.use(Pinia.createPinia());
 
