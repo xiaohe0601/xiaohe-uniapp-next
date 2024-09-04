0cdeb2f9aa47cc86cdf83010ba57a349b3da5985
xiaohe0601 <xiaohe0601@outlook.com>
2024-09-04 16:56:42 +0800

    chore: vite config code style

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,54 @@ import UniManifest from "@uni-helper/vite-plugin-uni-manifest";
 import UniPages from "@uni-helper/vite-plugin-uni-pages";
 import UnoCSS from "unocss/vite";
 import AutoImport from "unplugin-auto-import/vite";
+import type { PluginOption, TerserOptions } from "vite";
 import { defineConfig, loadEnv } from "vite";
 
 function r(...paths: string[]) {
   return path.resolve(process.cwd(), ".", ...paths);
 }
 
+function buildPlugins(): PluginOption[] {
+  return [
+    UniManifest(),
+    UniPages({
+      dts: "types/uni-pages.d.ts",
+      mergePages: false
+    }),
+    UniComponents({
+      dts: "types/components.d.ts",
+      dirs: ["src/components"],
+      directoryAsNamespace: true,
+      collapseSamePrefixes: true
+    }),
+    AutoImport({
+      dts: "types/auto-imports.d.ts",
+      imports: ["vue", "uni-app", "pinia"]
+    }),
+    UnoCSS(),
+    // @ts-expect-error whatever
+    UniApp.default()
+  ];
+}
+
+function buildTerserOptions(mode: string, env: Record<string, string>) {
+  const options: TerserOptions = {};
+
+  if (mode === "development") {
+    options.compress = false;
+  } else {
+    options.compress = {
+      keep_infinity: true,
+      drop_console: env.VITE_ENABLE_DROP_CONSOLE === "true",
+      drop_debugger: true
+    };
+  }
+
+  return options;
+}
+
 export default defineConfig(({ mode }) => {
   const env: Record<string, string> = loadEnv(mode, process.cwd());
-
-  const compress = ["production", "preview"].includes(mode);
 
   return {
     resolve: {
@@ -22,39 +60,14 @@ export default defineConfig(({ mode }) => {
         "@": r("src")
       }
     },
-    plugins: [
-      UniManifest(),
-      UniPages({
-        dts: "types/uni-pages.d.ts",
-        mergePages: false
-      }),
-      UniComponents({
-        dts: "types/components.d.ts",
-        dirs: ["src/components"],
-        directoryAsNamespace: true,
-        collapseSamePrefixes: true
-      }),
-      AutoImport({
-        dts: "types/auto-imports.d.ts",
-        imports: ["vue", "uni-app", "pinia"]
-      }),
-      UnoCSS(),
-      // @ts-expect-error whatever
-      UniApp.default()
-    ],
+    plugins: buildPlugins(),
     server: {
       host: true,
       port: 5173
     },
     build: {
       minify: "terser",
-      terserOptions: {
-        compress: compress && {
-          keep_infinity: true,
-          drop_console: env.VITE_ENABLE_DROP_CONSOLE === "true",
-          drop_debugger: true
-        }
-      }
+      terserOptions: buildTerserOptions(mode, env)
     }
   };
 });
