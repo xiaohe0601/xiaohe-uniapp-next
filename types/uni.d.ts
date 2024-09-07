b9821c6742ed57bdff45e4b92bb92ce38bc9a461
xiaohe0601 <xiaohe0601@outlook.com>
2024-09-07 20:30:15 +0800

    chore: declare global PageComponentInstance

--- types/uni.d.ts
+++ types/uni.d.ts
@@ -1,7 +1,14 @@
+import type { EventChannel } from "@dcloudio/uni-shared";
+import type { ComponentPublicInstance } from "vue";
+
 declare global {
   interface Uni extends UniNamespace.Uni {
     $zp: ZPagingGlobal;
   }
+
+  type PageComponentInstance = ComponentPublicInstance & {
+    getOpenerEventChannel?: () => OptionalValue<EventChannel>;
+  };
 }
 
 export {};
