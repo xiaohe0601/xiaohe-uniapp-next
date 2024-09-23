e5a5c8c4491b500da23a2e6816aa294f65f4e62a
xiaohe0601 <xiaohe0601@outlook.com>
2024-09-23 15:59:23 +0800

    chore: code style

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -229,6 +229,7 @@ instance.interceptors.response.use(async (res: UnResponse) => {
       response.data = JSON.parse(response.data);
     } catch (error) {
       console.error("JSON解析异常", error);
+
       return Promise.reject(response);
     }
   }
