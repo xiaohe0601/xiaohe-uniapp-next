9ebda15fddab8351e31ed4297c79c472eeb25cad
xiaohe0601 <xiaohe0601@outlook.com>
2024-11-07 14:30:27 +0800

    feat: getSystemInfo methods

--- src/utils/support.ts
+++ src/utils/support.ts
@@ -1,4 +1,17 @@
 /**
+ * 获取设备基础信息
+ *
+ * @see [uni.getDeviceInfo](https://uniapp.dcloud.net.cn/api/system/getDeviceInfo.html)
+ */
+export function getDeviceInfo() {
+  if (uni.getDeviceInfo && uni.canIUse("getDeviceInfo")) {
+    return uni.getDeviceInfo();
+  } else {
+    return uni.getSystemInfoSync();
+  }
+}
+
+/**
  * 获取窗口信息
  *
  * @see [uni.getWindowInfo](https://uniapp.dcloud.net.cn/api/system/getWindowInfo.html)
@@ -6,6 +19,45 @@
 export function getWindowInfo() {
   if (uni.getWindowInfo && uni.canIUse("getWindowInfo")) {
     return uni.getWindowInfo();
+  } else {
+    return uni.getSystemInfoSync();
+  }
+}
+
+/**
+ * 获取APP基础信息
+ *
+ * @see [uni.getAppBaseInfo](https://uniapp.dcloud.net.cn/api/system/getAppBaseInfo.html)
+ */
+export function getAppBaseInfo() {
+  if (uni.getAppBaseInfo && uni.canIUse("getAppBaseInfo")) {
+    return uni.getAppBaseInfo();
+  } else {
+    return uni.getSystemInfoSync();
+  }
+}
+
+/**
+ * 获取APP授权设置
+ *
+ * @see [uni.getAppAuthorizeSetting](https://uniapp.dcloud.net.cn/api/system/getappauthorizesetting.html)
+ */
+export function getAppAuthorizeSetting() {
+  if (uni.getAppAuthorizeSetting && uni.canIUse("getAppAuthorizeSetting")) {
+    return uni.getAppAuthorizeSetting();
+  } else {
+    return uni.getSystemInfoSync();
+  }
+}
+
+/**
+ * 获取设备设置
+ *
+ * @see [uni.getSystemSetting](https://uniapp.dcloud.net.cn/api/system/getsystemsetting.html)
+ */
+export function getSystemSetting() {
+  if (uni.getSystemSetting && uni.canIUse("getSystemSetting")) {
+    return uni.getSystemSetting();
   } else {
     return uni.getSystemInfoSync();
   }
