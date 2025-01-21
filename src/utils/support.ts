f812085bc6389b5a9bef738df457a5843149378e
xiaohe0601 <xiaohe0601@outlook.com>
2025-01-21 15:56:27 +0800

    fix: support.ts
    (cherry picked from commit c3c636c8fa317659c972223bb75494b7b05dead6)

--- src/utils/support.ts
+++ src/utils/support.ts
@@ -4,7 +4,7 @@
  * @see [uni.getDeviceInfo](https://uniapp.dcloud.net.cn/api/system/getDeviceInfo.html)
  */
 export function getDeviceInfo() {
-  if (uni.getDeviceInfo && uni.canIUse("getDeviceInfo")) {
+  if (uni.canIUse("getDeviceInfo") || uni.getDeviceInfo) {
     return uni.getDeviceInfo();
   } else {
     return uni.getSystemInfoSync();
@@ -17,7 +17,7 @@ export function getDeviceInfo() {
  * @see [uni.getWindowInfo](https://uniapp.dcloud.net.cn/api/system/getWindowInfo.html)
  */
 export function getWindowInfo() {
-  if (uni.getWindowInfo && uni.canIUse("getWindowInfo")) {
+  if (uni.canIUse("getWindowInfo") || uni.getWindowInfo) {
     return uni.getWindowInfo();
   } else {
     return uni.getSystemInfoSync();
@@ -30,7 +30,7 @@ export function getWindowInfo() {
  * @see [uni.getAppBaseInfo](https://uniapp.dcloud.net.cn/api/system/getAppBaseInfo.html)
  */
 export function getAppBaseInfo() {
-  if (uni.getAppBaseInfo && uni.canIUse("getAppBaseInfo")) {
+  if (uni.canIUse("getAppBaseInfo") || uni.getAppBaseInfo) {
     return uni.getAppBaseInfo();
   } else {
     return uni.getSystemInfoSync();
@@ -43,7 +43,7 @@ export function getAppBaseInfo() {
  * @see [uni.getAppAuthorizeSetting](https://uniapp.dcloud.net.cn/api/system/getappauthorizesetting.html)
  */
 export function getAppAuthorizeSetting() {
-  if (uni.getAppAuthorizeSetting && uni.canIUse("getAppAuthorizeSetting")) {
+  if (uni.canIUse("getAppAuthorizeSetting") || uni.getAppAuthorizeSetting) {
     return uni.getAppAuthorizeSetting();
   } else {
     return uni.getSystemInfoSync();
@@ -56,7 +56,7 @@ export function getAppAuthorizeSetting() {
  * @see [uni.getSystemSetting](https://uniapp.dcloud.net.cn/api/system/getsystemsetting.html)
  */
 export function getSystemSetting() {
-  if (uni.getSystemSetting && uni.canIUse("getSystemSetting")) {
+  if (uni.canIUse("getSystemSetting") || uni.getSystemSetting) {
     return uni.getSystemSetting();
   } else {
     return uni.getSystemInfoSync();
