815bc747ed348b4831571c9c9f0f9d252432234b
xiaohe0601 <xiaohe0601@outlook.com>
2026-01-28 16:48:34 +0800

    chore: update

--- src/components/AppPage/index.vue
+++ src/components/AppPage/index.vue
@@ -10,7 +10,8 @@ import type { Props, Slots } from "./types";
 defineOptions({
   name: "AppPage",
   options: {
-    virtualHost: true
+    virtualHost: true,
+    styleIsolation: "shared"
   }
 });
 
